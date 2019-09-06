1712a0b56cdb11eb733a516f4baff367206466af
Gabriella-Kate <gabriellakate04@gmail.com>
2019-09-06 23:05:36 +0200

    remove row functionality, messing around with available budget functionality

--- budget_app/app.js
+++ budget_app/app.js
@@ -4,35 +4,62 @@ function createTable() {
     let selection = document.getElementById('select').value;
     let desc = document.getElementById('description').value;
     let amount = document.getElementById('amount').value;
-
     let incomeTable = document.getElementById('income-table');
     let expenseTable = document.getElementById('expense-table');
 
     let row = document.createElement('tr');
-    let rowItem1 = document.createElement('td')
-    let rowItem2 = document.createElement('td');;
+    let rowItem1 = document.createElement('td');
+    let rowItem2 = document.createElement('td');
+    let rowItem3 = document.createElement('td');
     let rowItemText1 = document.createTextNode(desc);
     let rowItemText2 = document.createTextNode(amount);
+    let rowItemText3 = document.createElement('input');
+    rowItemText3.setAttribute('type', 'button');
+    rowItemText3.setAttribute('value', 'X');
+    rowItemText3.className = "remove";
 
     rowItem1.appendChild(rowItemText1);
     rowItem2.appendChild(rowItemText2);
+    rowItem3.appendChild(rowItemText3);
     row.appendChild(rowItem1);
     row.appendChild(rowItem2);
+    row.appendChild(rowItem3);
 
-    if (selection === 'plus') {
-        rowItem2.textContent = '+ ' + amount;
+    if (selection === 'plus' && desc !== '' && amount !== '') {
+        // rowItem2.textContent = '+ ' + amount;
         rowItem2.className = 'income-number';
+        rowItem2.setAttribute('name', 'credit');
         incomeTable.appendChild(row);
-    } else if (selection === 'minus') {
-        rowItem2.textContent = '- ' + amount;
+    } else if (selection === 'minus' && desc !== '' && amount !== '') {
+        // rowItem2.textContent = '- ' + amount;
         rowItem2.className = 'expense-number';
+        rowItem2.setAttribute('name', 'debit');
         expenseTable.appendChild(row);
     }
     else {
-        alert('Please select from the dropdown first')
+        alert('Please select from the dropdown and fill in your data')
     }
+    // start here tomorrow
+
+    let myList = getCellTextList($('#example'));
+    console.log(myList);
+
+    x = 0
+    for (var i = 0; i < myList.length; i++) {
+        y = parseInt(myList[i]);
+        x = x +y;
+    }
+    console.log(x);
+
+
+
+
+
+
+
+
     clearInputs();
-    calculateAvailableBudget();
+    removeRow();
 }
 
 function clearInputs() {
@@ -41,6 +68,28 @@ function clearInputs() {
     document.getElementById('amount').value = '';
 }
 
-function calculateAvailableBudget(){
-    document.getElementById('available-budget').textContent = 'gabi';
+function removeRow() {
+    $('table').on('click', 'input[type="button"]', function(e){
+       $(this).closest('tr').remove()
+    })
+}
+
+
+
+
+
+
+//
+function getCellTextList(tableElement) {
+      // Create an array
+      var textList = [];
+
+      // Iterate over each table row
+      tableElement.find('tbody tr').each(function() {
+          var row = $(this);
+          if (row.children('td').length > 0) {
+             textList.push(row.children('td').eq(1).text());
+          }
+      });
+      return textList;
 }
