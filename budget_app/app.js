3ce03c2320c1ea6b4ddee5faac3b0eeaf8c28ded
Gabriella-Kate <gabriellakate04@gmail.com>
2019-09-09 18:16:02 +0200

    totals visible on side, updating when deleting row, ui fixes

--- budget_app/app.js
+++ budget_app/app.js
@@ -1,5 +1,7 @@
 document.getElementById('submit').addEventListener('click', createTable);
 
+getMonth();
+
 function createTable() {
     let selection = document.getElementById('select').value;
     let desc = document.getElementById('description').value;
@@ -28,37 +30,19 @@ function createTable() {
     if (selection === 'plus' && desc !== '' && amount !== '') {
         // rowItem2.textContent = '+ ' + amount;
         rowItem2.className = 'income-number';
-        rowItem2.setAttribute('name', 'credit');
+        // rowItem2.setAttribute('name', 'credit');
         incomeTable.appendChild(row);
     } else if (selection === 'minus' && desc !== '' && amount !== '') {
         // rowItem2.textContent = '- ' + amount;
         rowItem2.className = 'expense-number';
-        rowItem2.setAttribute('name', 'debit');
+        // rowItem2.setAttribute('name', 'debit');
         expenseTable.appendChild(row);
     }
     else {
         alert('Please select from the dropdown and fill in your data')
     }
-    // start here tomorrow
-
-    let myList = getCellTextList($('#example'));
-    console.log(myList);
-
-    x = 0
-    for (var i = 0; i < myList.length; i++) {
-        y = parseInt(myList[i]);
-        x = x +y;
-    }
-    console.log(x);
-
-
-
-
-
-
-
-
     clearInputs();
+    calculateTotals();
     removeRow();
 }
 
@@ -71,25 +55,75 @@ function clearInputs() {
 function removeRow() {
     $('table').on('click', 'input[type="button"]', function(e){
        $(this).closest('tr').remove()
+       calculateTotals();
     })
 }
 
-
-
-
-
-
-//
 function getCellTextList(tableElement) {
-      // Create an array
-      var textList = [];
-
+      let textList = [];
       // Iterate over each table row
       tableElement.find('tbody tr').each(function() {
-          var row = $(this);
+          let row = $(this);
           if (row.children('td').length > 0) {
              textList.push(row.children('td').eq(1).text());
           }
       });
       return textList;
+}
+
+function calculateArrayElements(array) {
+    let x = 0;
+    let result;
+
+    for (let i = 0; i < array.length; i++) {
+        y = parseFloat(array[i]);
+        x = x + y;
+    }
+    return x;
+}
+
+function calculateTotals() {
+    let incomeList = getCellTextList($('#positive'));
+    let expenseList = getCellTextList($('#negative'));
+
+    let pos = calculateArrayElements(incomeList);
+    let neg = calculateArrayElements(expenseList);
+
+    let totalIncome = document.getElementById('total-income');
+    totalIncome.textContent = pos;
+    totalIncome.classList.add('budget-good');
+
+    let totalExpenses = document.getElementById('total-expenses');
+    totalExpenses.textContent = neg;
+    totalExpenses.classList.add('budget-bad');
+
+    let budget = pos - neg;
+
+    let available = document.getElementById('available-budget');
+    available.textContent = budget;
+
+    if (budget > 0) {
+        available.classList.add('budget-good');
+    }
+}
+
+function getMonth() {
+    let month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    let d = new Date();
+    let n = month[d.getMonth()];
+    let y = d.getFullYear();
+    document.getElementById('time').innerHTML = n + ' ' + y;
 }
